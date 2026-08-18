33baa94f83ef9de03aadbf53ce18b5224b69c301
Tuan <trantuantu96@yahoo.de>
2026-08-18 18:26:43 +0200

    add persistence repository and test

--- src/test/java/de/tuantu/playbacklog/TestcontainersConfiguration.java
+++ src/test/java/de/tuantu/playbacklog/TestcontainersConfiguration.java
@@ -1,10 +1,6 @@
 package de.tuantu.playbacklog;
 
 import org.springframework.boot.test.context.TestConfiguration;
-import org.springframework.boot.testcontainers.service.connection.ServiceConnection;
-import org.springframework.context.annotation.Bean;
-import org.testcontainers.postgresql.PostgreSQLContainer;
-import org.testcontainers.utility.DockerImageName;
 
 @TestConfiguration(proxyBeanMethods = false)
 class TestcontainersConfiguration {
