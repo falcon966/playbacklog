bad7015bca3bb2847690cbf1a36ab0abc474b466
Tuan <trantuantu96@yahoo.de>
2026-08-18 21:52:13 +0200

    add batch job service and change test to use it

--- src/test/java/de/tuantu/playbacklog/batch/PlaybackLogBatchJobIT.java
+++ src/test/java/de/tuantu/playbacklog/batch/PlaybackLogBatchJobIT.java
@@ -8,9 +8,6 @@ import de.tuantu.playbacklog.shared.StorageType;
 import org.junit.jupiter.api.Test;
 import org.springframework.batch.core.BatchStatus;
 import org.springframework.batch.core.job.JobExecution;
-import org.springframework.batch.core.job.parameters.JobParameters;
-import org.springframework.batch.core.job.parameters.JobParametersBuilder;
-import org.springframework.batch.test.JobOperatorTestUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.batch.test.context.SpringBatchTest;
 
